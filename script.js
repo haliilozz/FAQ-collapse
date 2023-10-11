13acc2193b657b814b20e1eac7eb29a45117bf2a
haliilozz <halil447oz@gmail.com>
2023-10-11 15:19:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const jokes = document.querySelectorAll(".joke");
+
+console.log(jokes);
+
+jokes.forEach((joke) => {
+  joke.addEventListener("click", function () {
+    joke.classList.toggle("active");
+  });
+});
